c43ae704a9e700fa5aac4819f4d7d7935ffd29fa
RidRed <99220163+RidRed@users.noreply.github.com>
2022-11-01 10:51:38 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -9,10 +9,10 @@ function ChangeColor()
 {
     var text = document.getElementById("textbox2").value;
     var articleDiv = document.querySelector("div.Main2");
+    var articleDiv2 = document.querySelector("a.text");
     articleDiv.removeAttribute("style");
     articleDiv.setAttribute("style", "background-color:"+ text);
-    
-    var articleDiv2 = document.querySelector("a.text");
+  
     articleDiv2.removeAttribute("style");
     articleDiv2.setAttribute("style", "color:"+ text);
     
