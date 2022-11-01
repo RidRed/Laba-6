9ec5249a0cb7c4cf445f5e14e65eb7770df0a88b
RidRed <99220163+RidRed@users.noreply.github.com>
2022-11-01 10:47:33 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -11,7 +11,10 @@ function ChangeColor()
     var articleDiv = document.querySelector("div.Main2");
     articleDiv.removeAttribute("style");
     articleDiv.setAttribute("style", "background-color:"+ text);
-
+    
+    var articleDiv2 = document.querySelector("a.text");
+    articleDiv2.removeAttribute("style");
+    articleDiv2.setAttribute("style", "background-color:"+ text);
     
 }
 
